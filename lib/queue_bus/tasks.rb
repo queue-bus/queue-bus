c700254d8f15b24561563a30fde021cd7f605100
Brian Leonard <brian@bleonard.com>
2015-02-04 22:06:27 -0800

    adds rake tasks and read

---
+++ lib/queue_bus/tasks.rb
@@ -0,0 +1,25 @@
+# require 'queue_bus/tasks'
+# will give you these tasks
+
+namespace :queuebus do
+
+  desc "Subscribes this application to QueueBus events"
+  task :subscribe => [ :preload ] do
+    manager = ::QueueBus::TaskManager.new(true)
+    count = manager.subscribe!
+    raise "No subscriptions created" if count == 0
+  end
+
+  desc "Unsubscribes this application from QueueBus events"
+  task :unsubscribe => [ :preload ] do
+    manager = ::QueueBus::TaskManager.new(true)
+    count = manager.unsubscribe!
+    puts "No subscriptions unsubscribed" if count == 0
+  end
+
+  # Preload app files if this is Rails
+  # you can also do this to load the right things
+  task :preload do
+    require 'queue-bus'
+  end
+end
--- lib/queue_bus/version.rb
+++ lib/queue_bus/version.rb
@@ -1,3 +1,3 @@
 module QueueBus
-  VERSION = "0.5.3"
+  VERSION = "0.5.4"
 end
