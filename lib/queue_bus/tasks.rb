8323cac71f3a4c6782c252984fd2dfecd2dda608
David Verba <davidv@taskrabbit.com>
2019-12-03 13:42:31 -0800

    add rake task for csv output of scheduled tasks

--- lib/queue_bus/tasks.rb
+++ lib/queue_bus/tasks.rb
@@ -24,6 +24,17 @@ namespace :queuebus do
     puts queues.join(", ")
   end
 
+  desc "list time based subscriptions"
+  task :list_scheduled => [ :preload ] do
+    scheduled_list = QueueBus::Application.all.flat_map do |app|
+      app.send(:subscriptions).all
+        .select { |s| s.matcher.filters['bus_event_type'] == 'heartbeat_minutes'  }
+    end
+    scheduled_text_list = scheduled_list.collect { |e| [ e.key, e.matcher.filters['hour'] || "*", e.matcher.filters['minute'] || "*" ] }
+    puts 'key, hour, minute'
+    puts scheduled_text_list.sort_by { |(_, hour, minute)| [hour.to_i, minute.to_i] }.map(&:to_csv)
+  end
+
   # Preload app files if this is Rails
   # you can also do this to load the right things
   task :preload do
